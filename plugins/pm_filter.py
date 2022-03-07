e5a1c9ec96b8c79e0cc40871e2ddd2a9212928b0
Albin <95078987+Samantha-a@users.noreply.github.com>
2022-03-07 20:52:53 +0530

    Fix

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -694,10 +694,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ],[
             InlineKeyboardButton(' 𝙾𝚠𝚗𝚎𝚛 ', url='https://t.me/albintko'),
             InlineKeyboardButton(' 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 ', url='https://t.me/+AMHw_K1wvOM3MTU9')
-        ]]
-        await message.reply_chat_action("typing")
-        m=await message.reply_sticker("CAACAgUAAxkBAAEOha9iJh88DjhbAAHMnlBkOb5DsjaPcNwAAgMDAAI6ZolXwVJ2tZvuCnQjBA") 
-        await asyncio.sleep(1) await m.delete()
+        ]]        
          reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
